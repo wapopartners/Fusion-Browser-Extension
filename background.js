11f42533fe4ccda0aa22a3fdacc342518fe39f71
JackHowa <jack.howard@washpost.com>
2021-05-04 12:04:23 -0500

    Receive message via sw

--- background.js
+++ background.js
@@ -1,11 +1,5 @@
-chrome.action.onClicked.addListener((tab) => {
-  // https://developer.chrome.com/docs/extensions/reference/action/#event-onClicked
-  // will not fire if the action has a default popup in manifest.json
+chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
+  console.log('received message', message)
 
-  // enable scripting in manifest permissions
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    // currently only one file allowed
-    files: ['inject.js']
-  });
+  sendResponse('got the message ty content.js')
 });
