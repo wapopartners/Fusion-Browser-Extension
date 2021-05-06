6ed0b104820e57be0c4c1bfd3ee1afe8b96ebca2
JackHowa <jack.howard@washpost.com>
2021-05-06 17:45:41 -0500

    feat: Basic poc for changing tabs

---
+++ public/background.js
@@ -0,0 +1,15 @@
+// via https://www.reddit.com/r/learnjavascript/comments/7sbiyh/listener_for_chrometabsonactivated_never_called/dt3vyr6/?utm_source=reddit&utm_medium=web2x&context=3
+(function() {
+  this.currentTabId = undefined;
+
+  // https://developer.chrome.com/docs/extensions/reference/tabs/#event-onActivated
+  chrome.tabs.onActivated.addListener(({ tabId }) => {
+    if (tabId !== this.currentTabId) {
+      // set this for listening again
+      this.currentTabId = tabId;
+
+      // set the new current popup
+      chrome.action.setPopup({ popup: 'refresh-page.html', tabId })
+    }
+  });
+})();
