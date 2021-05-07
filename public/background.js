7fedd2a429ab901276de1bf1f454a19ae3af12b1
JackHowa <jack.howard@washpost.com>
2021-05-07 13:06:48 -0500

    style: Fix linting warning for chrome.* apis

--- public/background.js
+++ public/background.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // via https://www.reddit.com/r/learnjavascript/comments/7sbiyh/listener_for_chrometabsonactivated_never_called/dt3vyr6/?utm_source=reddit&utm_medium=web2x&context=3
 (function() {
   this.currentTabId = undefined;
@@ -8,7 +9,6 @@
       // set this for listening again
       this.currentTabId = tabId;
 
-      // clear storage on tab change
       chrome.storage.sync.clear();
 
       // set the new current popup
